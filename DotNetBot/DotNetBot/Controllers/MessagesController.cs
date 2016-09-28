b23c309566883b38e91ed2bbf2668f72cf33e6f6
ericpopivker <eric@entechsolutions.com>
2016-09-28 13:44:12 +0200

    First push

---
+++ DotNetBot/DotNetBot/Controllers/MessagesController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace DotNetBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                
+                // calculate something for us to return
+                //int length = (activity.Text ?? string.Empty).Length;
+
+                //return our reply to the user
+                //Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                var roslynSession = RoslynSession.LoadOrCreate(activity.Conversation.Id);
+
+
+                string replyText = String.Empty;
+
+                switch (activity.Text)
+                {
+                    case "#help":
+                        roslynSession.Reset();
+                        replyText = "\\#help - Sorry I don't have self awareness." + Environment.NewLine + Environment.NewLine;
+                        replyText += "\\#reset - Reset the execution environment to the initial state, keep history.";
+
+                        break;
+
+                    case "#reset":
+                        roslynSession.Reset();
+                        replyText = "I'm melting, melting. Ohhhhh, what a world, what a world.";
+                        break;
+
+                    default:
+                        string returnValue = await roslynSession.AddAndExecuteCodeEntryAsync(activity.Text);
+                        if (returnValue == null)
+                            returnValue = GetEmptyPlaceholder();
+                        replyText = returnValue;
+                        break;
+                }
+
+
+                Activity reply = activity.CreateReply(replyText);
+
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                var reply = HandleSystemMessage(activity);
+                if (reply != null)
+                {
+                    ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+
+        static Random _rnd = new Random();
+
+        private static List<string> _emptyPlaceholders = new List<string>
+        {
+            "Hmmm...",
+            "Zzzzz...",
+            "Make up yoir mind, friend.",
+            "Wait let me call Billy G...",
+            "I got nothing."
+        };
+
+
+
+        private string GetEmptyPlaceholder()
+        {
+            int index = _rnd.Next(_emptyPlaceholders.Count);
+            return _emptyPlaceholders[index];
+        }
+
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+                if (message.MembersAdded != null)
+                {
+                    string replyText = "I am .NET Bot (v 0.9). ";
+                    replyText += "I like compilers, syntact trees and long walks on the beach." + Environment.NewLine + Environment.NewLine;
+                    replyText += "Type '#help' for more information.";
+
+                    Activity reply = message.CreateReply(replyText);
+                    reply.Recipient = message.MembersAdded[0];
+
+                    return reply;
+                }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+               
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing that the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+ 
+            return null;
+        }
+    }
+}
---
+++ DotNetBot/DotNetBot/RoslynSession.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Caching;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace DotNetBot
+{
+    public class RoslynSession
+    {
+        private List<string> _codeEntries;
+
+        public string SessionId { get; set; }
+
+        public List<string> CodeEntries
+        {
+            get
+            {
+                return _codeEntries;
+            }
+            
+        }
+
+        private RoslynSession(string sessionId)
+        {
+            _codeEntries = new List<string>();
+
+            SessionId = sessionId;
+        }
+
+
+        public static RoslynSession Create(string sessionId)
+        {
+            var session = new RoslynSession(sessionId);
+            session.AddToCache();
+            return session;
+
+        }
+
+
+        public static RoslynSession LoadOrCreate(string sessionId)
+        {
+            var session = Load(sessionId);
+            if (session == null)
+                session = Create(sessionId);
+            return session;
+        }
+
+
+        public static RoslynSession Load(string sessionId)
+        {
+            var obj = System.Web.HttpRuntime.Cache.Get(sessionId);
+            return obj as RoslynSession;
+        }
+
+        public static void Remove(Guid sessionId)
+        {
+            System.Web.HttpRuntime.Cache.Remove(sessionId.ToString());
+        }
+
+        private void AddToCache()
+        {
+            string key = SessionId.ToString();
+            System.Web.HttpRuntime.Cache.Add(key, this, null, Cache.NoAbsoluteExpiration,  new TimeSpan(1, 0, 0), CacheItemPriority.High, null);
+        }
+
+
+        public async Task<string> AddAndExecuteCodeEntryAsync(string code)
+        {
+            string returnValue = String.Empty;
+            ScriptState state = null;
+
+            _codeEntries.Add(code);
+
+            for (int i = 0; i < _codeEntries.Count; i++)
+            {
+                try
+                {
+                    state = await RunCodeEntry(_codeEntries[i], state);
+                    returnValue = state.ReturnValue?.ToString();
+                }
+                catch (Exception ex)
+                {
+                    returnValue = ex.Message;
+                }
+            }
+
+            return returnValue;
+        }
+
+        private async Task<ScriptState> RunCodeEntry(string code, ScriptState state)
+        {
+            if (state == null)
+                state = await CSharpScript.RunAsync(code);
+            else
+                state = await state.ContinueWithAsync(code);
+
+            return state;
+        }
+
+
+        public void Reset()
+        {
+            _codeEntries.Clear();
+        }
+    }
+}
